9f48660e600c51e0cc30daefdbabad36570f1f2d
Rubel Mahmud <vxlrubel@gmail.com>
2024-11-04 01:27:14 +0600

    update registration authenticatio.

--- inc/Api/RegisterUser.php
+++ inc/Api/RegisterUser.php
@@ -8,12 +8,22 @@ use WP_REST_Server;
 use WP_REST_Controller;
 
 class RegisterUser extends WP_REST_Controller {
-
+    
+    /**
+     * Method __construct
+     *
+     * @return void
+     */
     public function __construct(){
         $this->namespace = 'vchat/v1';
         $this->rest_base = '/register';
     }
-
+    
+    /**
+     * Method register_routes
+     *
+     * @return void
+     */
     public function register_routes(){
         $args = [
             [
@@ -24,11 +34,38 @@ class RegisterUser extends WP_REST_Controller {
         ];
         register_rest_route( $this->namespace, $this->rest_base, $args );
     }
-
+    
+    /**
+     * Method check_permission
+     *
+     * @return void
+     */
     public function check_permission(){
         return true;
     }
-
+        
+    /**
+     * Method check_empty_field
+     *
+     * @param string $name [This param use for define key of array]
+     * @param string $message [This field define value of array.]
+     *
+     * @return void
+     */
+    protected function check_empty_field( string $name = 'invalid', string $message = 'Asign invalid field.' ){
+        $args = [
+            $name => $message
+        ];
+        return $args;
+    }
+        
+    /**
+     * Method register_new_user
+     *
+     * @param $request $request [This param receive user information for create new user.]
+     *
+     * @return void
+     */
     public function register_new_user($request) {
         $params = $request->get_params();
     
@@ -38,6 +75,26 @@ class RegisterUser extends WP_REST_Controller {
             'password' => $params['password'],
             'username' => sanitize_user($params['username']),
         ];
+        
+        if( !$params['name'] && !$params['email'] && !$params['password'] && !$params['username'] ){
+            return 'Please provide the valid information.';
+        }
+
+        if( !$params['name'] ){
+            return $this->check_empty_field('name', 'Please insert name.');
+        }
+
+        if( !$params['email'] ){
+            return $this->check_empty_field('email', 'Please insert email.');
+        }
+
+        if( !$params['password'] ){
+            return $this->check_empty_field('password', 'Please insert password.');
+        }
+        
+        if( !$params['username'] ){
+            return $this->check_empty_field('username', 'Please insert username.');
+        }
     
         // Check if username already exists.
         if ( username_exists( $get_details['username']) ) {
@@ -83,6 +140,5 @@ class RegisterUser extends WP_REST_Controller {
             'message' => 'Registered successfully'
         ];
     }
-
     
 }
